0bf4c628b383beb5e6d893fe794b23c89b8dbe84
Rendra Suroso <rs@cogsci.bandungfe.net>
2019-07-14 03:42:29 +0700

    Backport dev/TestCase and src/Config from 2.1.

---
+++ src/Config.php
@@ -0,0 +1,328 @@
+<?php
+
+
+namespace BFITech\ZapCore;
+
+
+/**
+ * Configuration error class.
+ */
+class ConfigError extends \Exception {
+
+	/** Configuration file not found. */
+	const CNF_FILE_NOT_FOUND = 0x0101;
+	/** Key separator is invalid. */
+	const CNF_SEPARATOR_INVALID = 0x0102;
+	/** Configuration file is broken. */
+	const CNF_FILE_INVALID = 0x0103;
+
+	/** Key to retrive not found. */
+	const GET_KEY_NOT_FOUND = 0x0201;
+
+	/** Setting a key that has a subkey. */
+	const SET_HAS_SUBKEY = 0x0201;
+	/** Wrongly setting array to scalar value. */
+	const SET_VALUE_NOT_SCALAR = 0x0202;
+	/** Wrongly setting scalar to array value. */
+	const SET_VALUE_NOT_ARRAY = 0x0203;
+
+	/** New key for addition is too short. */
+	const ADD_KEY_TOO_SHORT = 0x0301;
+	/** New key for addition already exists. */
+	const ADD_KEY_FOUND = 0x0302;
+	/** New value for addition is an associative array */
+	const ADD_VALUE_IS_DICT = 0x0303;
+
+	/** Key for deletion doesn't exist. */
+	const DEL_KEY_NOT_FOUND = 0x0401;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param int $errno Error number.
+	 * @param string $errmsg Error message.
+	 */
+	public function __construct($errno, $errmsg) {
+		$this->code = $errno;
+		$this->message = $errmsg;
+	}
+
+}
+
+
+/**
+ * Configuration class.
+ *
+ * This is a generic class to set and get configuration values.
+ * Default backend is JSON, using `json_encode` and `json_decode`.
+ *
+ * You can implement your own backend with YAML, TOML, XML,
+ * or any other format as long as it supports JSON-like arbitrary data
+ * structure. You only need to patch Config::read() and Config::write()
+ * to do this.
+ *
+ * ### example
+ * @code
+ * <?php
+ *
+ * file_put_contents('config.json', '[]');
+ *
+ * $cnf = Config('config.json');
+ *
+ * $cnf->add('tom.yam.goong', 2);
+ * print_f($cnf->get('tom.yam');
+ * # prints ['goong' => 2]
+ *
+ * $cnf = Config('config.json', ':');
+ *
+ * $cnf->set('tom:yam', -1);
+ * # throws ConfigError
+ *
+ * $cnf->set('tom:yam', [null, false]);
+ * # throws ConfigError
+ *
+ * $cnf->set('tom:yam:goong', -1);
+ * print_f($cnf->get('tom:yam:goong');
+ * # prints -1
+ * @endcode
+ */
+class Config {
+
+	private $cfile;
+	private $clist;
+	private $sep;
+
+	/**
+	 * Valid separators.
+	 */
+	const SEPARATORS = '.,:;/-_';
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $cfile Configuration file.
+	 * @param string $sep Key-subkey separator.
+	 */
+	public function __construct($cfile, $sep='.') {
+		if (!file_exists($cfile))
+			$this->throw_error(
+				ConfigError::CNF_FILE_NOT_FOUND,
+				"Config file '$cfile' not found.");
+		$this->cfile = $cfile;
+
+		$seps = self::SEPARATORS;
+		if (strlen($sep) != 1 || strpos($seps, $sep) === false)
+			$this->throw_error(
+				ConfigError::CNF_SEPARATOR_INVALID,
+				"Allowed section separator: '$seps'.");
+		$this->sep = $sep;
+
+		$this->read();
+	}
+
+	/**
+	 * Standard JSON decoder.
+	 *
+	 * Only used in this class if you're using default $this->read().
+	 *
+	 * @note This method relies on `json_decode` under the hood. Numeric
+	 *     value in an array is always decoded as float, whether it's
+	 *     integer or actually float. However, if the value is not in an
+	 *     array, decoding works as expected, and even passes naive
+	 *     float comparison.
+	 *
+	 * @param string $json_str JSON string.
+	 * @param bool $throw Throw exception on decoding error.
+	 * @return array|null Decoded JSON on success, null on failure.
+	 * @throws Exception on decoding error if $throw is set to true.
+	 */
+	public static function djson($json_str, $throw=false) {
+		$data = json_decode($json_str, true);
+		if ($data === null && $throw)
+			throw new \Exception("Invalid JSON file.");
+		return $data;
+	}
+
+	/**
+	 * Read from configuration JSON file.
+	 *
+	 * Patch this if you want to implement your own storage backend.
+	 */
+	protected function read() {
+		$cfile = $this->cfile;
+		try {
+			$clist = self::djson(file_get_contents($cfile), true);
+		} catch(\Exception $err) {
+			$this->throw_error(
+				ConfigError::CNF_FILE_INVALID,
+				"Config file '$cfile' broken or invalid.");
+		}
+		$this->clist = $clist;
+	}
+
+	/**
+	 * Write to configuration JSON file.
+	 *
+	 * Patch this if you want to implement your own storage backend.
+	 */
+	protected function write() {
+		file_put_contents(
+			$this->cfile,
+			json_encode($this->clist,
+				JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+	}
+
+	private function is_dict(array $arr) {
+		return array_keys($arr) !== range(0, count($arr) - 1);
+	}
+
+	private function throw_error($errno, $errmsg) {
+		throw new ConfigError($errno, $errmsg);
+	}
+
+	/**
+	 * Get a value.
+	 *
+	 * @param string $key Keys joined by separator.
+	 * @return mixed Configuration value. If key is null, all
+	 *     configuration is returned.
+	 * @throws ConfigError when key is invalid.
+	 */
+	public final function get($key=null) {
+		if ($key === null)
+			return $this->clist;
+		$sdata = $this->clist;
+		$sep = $this->sep;
+		$skey = [];
+		$akey = explode($sep, $key);
+		while ($akey) {
+			$ckey = $akey[0];
+			$skey[] = $ckey;
+			if (!array_key_exists($ckey, $sdata))
+				$this->throw_error(
+					ConfigError::GET_KEY_NOT_FOUND,
+					sprintf("Invalid key '%s'.", implode($sep, $skey))
+				);
+			$sdata = $sdata[$ckey];
+			array_shift($akey);
+		}
+		return $sdata;
+	}
+
+	/**
+	 * Set a value.
+	 *
+	 * @param string $key Keys joined by separator.
+	 * @param mixed $value Configuration value. Only scalar and
+	 *     non-associative array are accepted.
+	 * @return void
+	 * @throws ConfigError when key or value is invalid.
+	 */
+	public final function set($key, $value) {
+		$sdata = $this->get($key);
+		if (is_array($sdata) && $this->is_dict($sdata)) {
+			$this->throw_error(
+				ConfigError::SET_HAS_SUBKEY,
+				"Key '$key' has subkey."
+			);
+		}
+		if (is_array($value) && !is_array($sdata)) {
+			$this->throw_error(
+				ConfigError::SET_VALUE_NOT_SCALAR,
+				"Key '$key' is an array."
+			);
+		}
+		if (!is_array($value) && is_array($sdata)) {
+			$this->throw_error(
+				ConfigError::SET_VALUE_NOT_ARRAY,
+				"Key '$key' is not an array."
+			);
+		}
+
+		$nval =& $this->clist;
+		$akey = explode($this->sep, $key);
+		while ($akey) {
+			$nval =& $nval[$akey[0]];
+			array_shift($akey);
+		}
+		$nval = $value;
+
+		$this->write();
+	}
+
+	/**
+	 * Add a key.
+	 *
+	 * @param string $key Keys joined by separator.
+	 * @param mixed $value Configuration value. Only scalar and
+	 *     non-associative array are accepted.
+	 * @return void
+	 * @throws ConfigError when key or value is invalid, or key already
+	 *     exists.
+	 */
+	public final function add($key, $value) {
+		if (strlen($key) < 1)
+			$this->throw_error(
+				ConfigError::ADD_KEY_TOO_SHORT,
+				"Key name is too short.");
+
+		if (is_array($value) && $this->is_dict($value))
+			$this->throw_error(
+				ConfigError::ADD_VALUE_IS_DICT,
+				"Value must not be associative array.");
+
+		$sdata = null;
+		try {
+			$sdata = $this->get($key);
+		} catch (ConfigError $err) {
+		}
+		if ($sdata)
+			$this->throw_error(
+				ConfigError::ADD_KEY_FOUND,
+				"Key '$key' already exists.");
+
+		$nval =& $this->clist;
+		$akey = explode($this->sep, $key);
+		while ($akey) {
+			$nval =& $nval[$akey[0]];
+			array_shift($akey);
+		}
+		$nval = $value;
+
+		$this->write();
+	}
+
+	/**
+	 * Remove a key.
+	 *
+	 * This removes the key even if it has subkeys under it.
+	 *
+	 * @param string $key Keys joined by separator.
+	 * @return void
+	 * @throws ConfigError when key is not found.
+	 *
+	 * @if TRUE
+	 * @SuppressWarnings(PHPMD.EvalExpression)
+	 * @endif
+	 */
+	public final function del($key) {
+		try {
+			$this->get($key);
+		} catch(ConfigError $err) {
+			$this->throw_error(
+				ConfigError::DEL_KEY_NOT_FOUND,
+				"Key '$key' not found.");
+		}
+
+		$skey = '';
+		$akey = explode($this->sep, $key);
+		while ($akey) {
+			$skey .= sprintf("['%s']", $akey[0]);
+			array_shift($akey);
+		}
+		eval('unset($this->clist' . $skey . ');');
+
+		$this->write();
+	}
+
+}
---
+++ tests/ConfigTest.php
@@ -0,0 +1,266 @@
+<?php
+
+
+use BFITech\ZapCore\Config;
+use BFITech\ZapCore\ConfigError;
+use BFITech\ZapCoreDev\RouterDev;
+use BFITech\ZapCoreDev\TestCase;
+
+
+class ConfigTest extends TestCase {
+
+	private static $cfile;
+
+	public function setUp() {
+		$this->data = [
+			's0' => [
+				'k0' => 'v0',
+				'k1' => null,
+				'k2' => 65535,
+				'k3' => [1e2, 1e2 + 1],
+				'k4' => M_PI,
+			],
+			's1' => [
+				'k0' => ['a', 'b', 1e3],
+			],
+		];
+		self::$cfile = self::tdir(__FILE__) . '/zapconfig.json';
+		file_put_contents(
+			self::$cfile, json_encode($this->data,
+				JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+	}
+
+	public function tearDown() {
+		unlink(self::$cfile);
+	}
+
+	public function test_constructor() {
+		extract($this->vars());
+
+		# file doesn't exist
+		$code = 0;
+		try {
+			new Config('/z/z/z');
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq($code, ConfigError::CNF_FILE_NOT_FOUND);
+
+		### create dummy file with non-standard key names
+		$cfile = self::tdir(__FILE__) . '/zapconfig-broken.json';
+		$cdata = [
+			's0.x' => [
+				'w:t' => 'xoxo',
+			],
+		];
+		file_put_contents($cfile, json_encode($cdata));
+
+		# separator invalid
+		$code = 0;
+		try {
+			new Config($cfile, 'a');
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq($code, ConfigError::CNF_SEPARATOR_INVALID);
+
+		# success
+		$cnf = new Config($cfile, '/');
+		$eq($cnf->get('s0.x/w:t'), 'xoxo');
+		$sm($cnf->get(), $cdata);
+
+		### break the file
+		file_put_contents($cfile, 'breakit', FILE_APPEND);
+
+		# decoding is broken
+		$broken = false;
+		try {
+			Config::djson($cfile, true);
+		} catch (\Exception $err) {
+			$broken = true;
+		}
+		$tr($broken);
+
+		# file is broken
+		$code = 0;
+		try {
+			new Config($cfile);
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$sm($code, ConfigError::CNF_FILE_INVALID);
+
+		unlink($cfile);
+	}
+
+	public function test_get() {
+		extract($this->vars());
+
+		$cnf = new Config(self::$cfile);
+
+		# null key
+		$sm($cnf->get()['s0']['k4'], M_PI);
+
+		# these arrays are equal
+		$eq($cnf->get('s1.k0'), ['a', 'b', 1e3]);
+		# these arrays are not the same because the last element was
+		# decoded as float.
+		$ns($cnf->get('s1.k0'), ['a', 'b', 1e3]);
+
+		# invalid key
+		$code = 0;
+		try {
+			$cnf->get('s3');
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::GET_KEY_NOT_FOUND, $code);
+		$code = 0;
+		try {
+			$cnf->get('s1.k1');
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::GET_KEY_NOT_FOUND, $code);
+
+		# string
+		$eq($cnf->get('s0.k0'), 'v0');
+
+		# null
+		$nil($cnf->get('s0.k1'));
+
+		# integer
+		$eq($cnf->get('s0.k2'), pow(2, 16) - 1);
+
+		# number inside array, always decoded to float by json_decode
+		$eq($cnf->get('s0.k3')[1], 1e2 + 1);
+		$ns($cnf->get('s0.k3')[1], 1e2 + 1);
+
+		# number
+		$sm($cnf->get('s0.k4'), M_PI);
+		$eq($cnf->get('s0.k4'), M_PI);
+	}
+
+	public function test_set() {
+		extract($this->vars());
+
+		$cnf = new Config(self::$cfile);
+
+		# setting key that has subkey
+		$code = 0;
+		try {
+			$cnf->set('s0', 2);
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::SET_HAS_SUBKEY, $code);
+
+		# invalid array value
+		$code = 0;
+		try {
+			$cnf->set('s1.k0', 2);
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::SET_VALUE_NOT_ARRAY, $code);
+
+		# invalid scalar value
+		$code = 0;
+		try {
+			$cnf->set('s0.k0', [2]);
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::SET_VALUE_NOT_SCALAR, $code);
+
+		# valid scalar value
+		$cnf->set('s0.k0', 'v0x');
+		### comparison with the same config instance
+		$sm($cnf->get('s0.k0'), 'v0x');
+
+		# valid array value
+		$cnf->set('s0.k3', ['x', 'y']);
+
+		# valid, by re-reading from config file
+		$ncnf = new Config(self::$cfile);
+		$sm($ncnf->get('s0.k3'), ['x', 'y']);
+	}
+
+	public function test_add() {
+		extract($this->vars());
+
+		$cnf = new Config(self::$cfile);
+
+		# invalid key
+		$code = 0;
+		try {
+			$cnf->add('', 2);
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::ADD_KEY_TOO_SHORT, $code);
+
+		# value is assoc
+		$code = 0;
+		try {
+			$cnf->add('s0.k7', ['a' => null]);
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::ADD_VALUE_IS_DICT, $code);
+
+		# key exists
+		$code = 0;
+		try {
+			$cnf->add('s0.k4', 2);
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::ADD_KEY_FOUND, $code);
+
+		# success
+		$cnf->add('s2.k3.l3', false);
+		$fl($cnf->get('s2.k3.l3'));
+
+		# success, with re-reading config file
+		$ncnf = new Config(self::$cfile);
+		$sm($ncnf->get('s2.k3.l3'), false);
+	}
+
+	public function test_del() {
+		extract($this->vars());
+
+		$cnf = new Config(self::$cfile);
+
+		# invalid key
+		$code = 0;
+		try {
+			$cnf->del('s3');
+		} catch (ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::DEL_KEY_NOT_FOUND, $code);
+
+		# success
+		$cnf->del('s0.k2');
+		$code = 0;
+		try {
+			$cnf->get('s0.k2');
+		} catch(ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::GET_KEY_NOT_FOUND, $code);
+
+		# success, with re-rading config file
+		$cnf->del('s0');
+		$code = 0;
+		$ncnf = new Config(self::$cfile);
+		try {
+			$ncnf->get('s0');
+		} catch(ConfigError $err) {
+			$code = $err->getCode();
+		}
+		$eq(ConfigError::GET_KEY_NOT_FOUND, $code);
+	}
+
+}
